b4a1be5e510843e158a0932bf798eca673b112c0
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-11-19 12:33:24 -0800

    🏗️ made layout of home

--- app/index.js
+++ app/index.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native';
+import React from 'react';
+import { Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { Dimensions } from 'react-native';
+import { COLORS, icons, images, SIZES } from '../constants';
+
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from '../components';
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: '',
+        }}
+      />
+
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
