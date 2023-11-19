c34df2e11c2f9f780ad92836abe96ebce2e80727
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-11-19 13:25:54 -0800

    ⬆️ upgrade expo sdk

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -22,7 +22,12 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
         </View>
       </View>
     </View>
