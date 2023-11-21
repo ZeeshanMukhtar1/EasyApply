22742bcf976dd0f94c1b1c3414551589b7d588a2
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-11-20 21:52:13 -0800

    🔥 added buggy popuar jobs card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-import styles from './popularjobcard.style'
+import styles from './popularjobcard.style';
 
-const PopularJobCard = () => {
+import checkImageUrl from '../../../../utils';
+
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{ uri: item.employer_logo }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <View>
+        <Text style={styles.companyName} numberOfLines={1}>
+          {item.employer_name}
+        </Text>
+        <View style={styles.infoContainer}>
+          <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+            {item.job_title}
+          </Text>
+          <Text style={styles.location}>{item.job_country}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
