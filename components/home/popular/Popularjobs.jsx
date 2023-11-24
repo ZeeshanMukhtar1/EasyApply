270a0f6f3cde7916ddbfb2f51a18c79b459e365e
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-11-23 21:51:50 -0800

    💬 Changes titles to modern text

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import { useRouter } from "expo-router";
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
 import {
   View,
   Text,
   TouchableOpacity,
   FlatList,
   ActivityIndicator,
-} from "react-native";
+} from 'react-native';
 
-import styles from "./popularjobs.style";
-import { COLORS, SIZES } from "../../../constants";
-import PopularJobCard from "../../common/cards/popular/PopularJobCard";
-import useFetch from "../../../hook/useFetch";
+import styles from './popularjobs.style';
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const { data, isLoading, error } = useFetch("search", {
-    query: "React developer",
-    num_pages: "1",
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: '1',
   });
 
   const [selectedJob, setSelectedJob] = useState();
@@ -30,7 +30,7 @@ const Popularjobs = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <Text style={styles.headerTitle}>Trending Jobs</Text>
         <TouchableOpacity>
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
@@ -38,9 +38,12 @@ const Popularjobs = () => {
 
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size='large' color={COLORS.primary} />
+          <ActivityIndicator size="large" color={COLORS.primary} />
         ) : error ? (
-          <Text>Something went wrong</Text>
+          <Text>
+            Something went wrong while fetching job data . Please try again
+            later.
+          </Text>
         ) : (
           <FlatList
             data={data}
